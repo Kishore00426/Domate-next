e781556a3a79a56b140a29b8c4c9f46c0d70f1a5
Kishore00426 <kishorefsdeveloper@gmail.com>
2026-03-11 15:36:10 +0530

    updated the store provider

--- src/store/StoreProvider.tsx
+++ src/store/StoreProvider.tsx
@@ -2,8 +2,15 @@
 
 import { Provider } from 'react-redux';
 import { store } from '@/store/store';
-import '@/i18n';
+import i18n from '@/i18n';
+import { I18nextProvider } from 'react-i18next';
 
 export function StoreProvider({ children }: { children: React.ReactNode }) {
-    return <Provider store={store}>{children}</Provider>;
+    return (
+        <Provider store={store}>
+            <I18nextProvider i18n={i18n}>
+                {children}
+            </I18nextProvider>
+        </Provider>
+    );
 }
