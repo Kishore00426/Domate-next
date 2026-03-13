14f10b0e63961d75b41e7ac379b93d5e6c52dc65
Kishore00426 <kishorefsdeveloper@gmail.com>
2026-03-13 16:02:18 +0530

    updated the files ssr

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,45 +1,7 @@
-'use client';
+import LandingClient from './LandingClient';
 
-import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-import Navbar from '@/components/Navbar';
-import Hero from '@/components/Hero';
-import HowItWorks from '@/components/HowItWorks';
-import WhyChooseUs from '@/components/WhyChooseUs';
-import Footer from '@/components/Footer';
-import GoToTop from '@/components/GoToTop';
-import { fetchServices } from '@/store/servicesSlice';
+export const dynamic = 'force-dynamic';
 
 export default function LandingPage() {
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    // @ts-ignore
-    dispatch(fetchServices());
-
-    // Redirect service providers to their dashboard
-    const userStr = localStorage.getItem('user');
-    if (userStr) {
-      try {
-        const user = JSON.parse(userStr);
-        const role = user?.role?.name || user?.role;
-        if (role === 'service_provider') {
-          window.location.href = '/provider/dashboard';
-        }
-      } catch (e) {
-        console.error("Failed to parse user data", e);
-      }
-    }
-  }, [dispatch]);
-
-  return (
-    <>
-      <Navbar />
-      <Hero />
-      <HowItWorks />
-      <WhyChooseUs />
-      <Footer />
-      <GoToTop />
-    </>
-  );
+  return <LandingClient />;
 }
